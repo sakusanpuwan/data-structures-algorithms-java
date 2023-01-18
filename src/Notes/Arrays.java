7379d04661a81898dcfb5ff895caa9a4298a987e
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-01-18 17:54:38 +0000

    added notes

---
+++ src/Notes/Arrays.java
@@ -0,0 +1,7 @@
+package Notes;
+
+public class Arrays {
+
+    int [] primes = {2,3,5,7,11};
+    int [] squares = new int[] {1,4,9,16};
+}
---
+++ src/Notes/Tips.java
@@ -0,0 +1,18 @@
+package Notes;
+
+public class Tips {
+
+    /*
+    Block comments
+     */
+
+    // Inline comments
+
+    // '/n' newline
+
+    // '/t' tab
+
+    // todo:message
+
+    
+}
