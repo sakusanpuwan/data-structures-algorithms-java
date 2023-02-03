326db48c4d155beebed1aa86d0f6b221d812388c
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-02-03 19:34:15 +0000

    Arge kata - while loop

---
+++ src/Katas/Arge.java
@@ -0,0 +1,17 @@
+package Katas;
+
+class Arge {
+
+    public static int nbYear(int p0, double percent, int aug, int p) {
+
+        int totalPop = p0;
+        int year = 0;
+
+        while (totalPop < p){
+            totalPop = (int) (totalPop + totalPop*percent/100 + aug);
+            year ++;
+        }
+
+        return year;
+    }
+}
