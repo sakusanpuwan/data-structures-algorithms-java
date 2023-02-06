51095ef61e5a3aa49d698678bc8908d19595df61
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-02-06 14:05:36 +0000

    solved binary addition kata - int to binary

---
+++ src/Katas/BinaryAddition.java
@@ -0,0 +1,11 @@
+package Katas;
+
+public class BinaryAddition {
+    public static String binaryAddition(int a, int b) {
+
+        int decimalResult = a + b;
+        String binaryResult = Integer.toBinaryString(decimalResult);
+
+        return binaryResult;
+    }
+}
