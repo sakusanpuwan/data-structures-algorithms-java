705164e6f88f2c6f819845bcde4255b3079094d2
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-02-07 13:21:39 +0000

    added TwoSum leetcode problem with notes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,6 @@
-import Katas.RowSumOddNumbers;
-
 public class Main {
     public static void main(String[] args) {
 
-//        int result = RowSumOddNumbers.rowSumOddNumbers(5);
-        System.out.println(3^2);
 
     }
 
