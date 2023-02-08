cc003a892b58633740b5e5123e837f6a316ab05d
sakusanpuwan <sakusanpuwan98@gmail.com>
2023-02-08 13:37:23 +0000

    added anagram leetcode problem

---
+++ src/LeetCode/HappyNumber.java
@@ -0,0 +1,28 @@
+package LeetCode;
+
+import java.util.HashSet;
+
+public class HappyNumber {
+    public boolean isHappy(int n){
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        int sum = 0;
+
+        while(sum != 1 && !hashSet.contains(sum)){
+            hashSet.add(sum);
+            sum = 0;
+
+            while (n != 0){
+                // 19%10 => 1 reminder 9  = 9
+                // 19/10 => 1.9 = 1
+                sum += Math.pow(n%10,2); // 9^2 1^2
+                n = n/10; // 1 0 -> break
+                // sum = 81 + 1 = 82
+            }
+
+            n = sum;
+        }
+
+        return sum == 1;
+    }
+}
